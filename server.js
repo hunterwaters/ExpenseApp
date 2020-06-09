f6fccde104328180aff4294b2234fe6e59bc3eea
hunterwaters <hunterrwaters@gmail.com>
2020-06-09 11:58:47 -0700

    make it better

--- server.js
+++ server.js
@@ -37,5 +37,13 @@ const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` .yellow.bold));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 module.exports = app
 
