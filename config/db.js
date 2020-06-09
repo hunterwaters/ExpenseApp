7ca0d09e7a57264926f5704f54d484eb37cf0932
hunterwaters <hunterrwaters@gmail.com>
2020-06-09 11:45:19 -0700

    make it better

--- config/db.js
+++ config/db.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose');
 
-
-
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://HunterWaters:Goldsgym1@expense-tracker-jjhbm.mongodb.net/expensetracker?retryWrites=true&w=majority", {
             useNewUrlParser: true,
             useCreateIndex: true,
             useUnifiedTopology: true
         });
 
         console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
-    } catch (err) {
+   } catch (err) {
         console.log(`Error: ${err.message}`.red);
         process.exit(1);
     }
 }
+console.log(process.env.MONGO_URI)
+
 
 module.exports = connectDB;
+
+
+
+
+
--- server.js
+++ server.js
@@ -32,6 +32,7 @@ if(process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
+
 const PORT = process.env.PORT || 2000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` .yellow.bold));
