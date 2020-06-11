da98529e31ee440656a0b9b01cad248e46638079
hunterwaters <hunterrwaters@gmail.com>
2020-06-11 13:23:09 -0700

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
-const colors = require('colors');
+//const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db')
 
